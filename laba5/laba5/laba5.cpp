871c4e0a7556ba54c4136a4190eb975d89aef4e1
Max <71633596+MFTOne@users.noreply.github.com>
2020-10-02 15:37:17 +0300

    MFTOne
    Ну вот так вот

---
+++ laba5/laba5/laba5.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+void main() {
+	setlocale(LC_CTYPE, "Russian");
+	int a, x, c, sum = 0;
+	cout << "Введите число x ";
+	cin >> x;
+	cout << "Введите число a (от 0 до 8) ";
+	cin >> a;
+	while (x) 
+	{
+	c = x % 10;
+	if (c > a) sum += c;
+	x /= 10;
+	}
+	cout << "Сумма цифр равна " << sum << endl;
+}
